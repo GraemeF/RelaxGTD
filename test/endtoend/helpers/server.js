2d5b599c530705aa6e92a7a9a4af81c73c35f44f
Graeme Foster <graemef@gmail.com>
2012-03-04 11:44:42 +0000

    Added stubs for the other bits that are now needed.

--- test/endtoend/helpers/server.js
+++ test/endtoend/helpers/server.js
@@ -1,11 +1,23 @@
-var config = require('../../../lib/config');
 var relax = require('../../../lib/relaxgtd');
+
+var config = {
+    "port":3005,
+    "session":{
+        "secret":"testing is fun"
+    },
+    "keys":{
+        "twitter":{
+            "consumerKey":"process.env.npm_package_config_twitterConsumerKey",
+            "consumerSecret":"process.env.npm_package_config_twitterConsumerSecret"
+        }
+    }
+};
 
 var Server = function () {
     var self = this;
 
     this.start = function () {
-        config.port = 3004;
+        config.port = 3005;
         self.relax = new relax(config);
         self.relax.start();
     };
